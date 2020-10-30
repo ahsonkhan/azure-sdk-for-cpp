2d8118738925aa7263b6ee9a47e8856ac0135ee0
Ahson Khan <ahkha@microsoft.com>
2020-10-30 13:53:15 -0700

    Update transport implementation.

--- sdk/core/azure-core/src/http/winhttp/win_http_transport.cpp
+++ sdk/core/azure-core/src/http/winhttp/win_http_transport.cpp
@@ -4,8 +4,9 @@
 #include "azure/core/http/http.hpp"
 #include "azure/core/http/winhttp/win_http_client.hpp"
 
-#include <string>
 #include <Winhttp.h>
+#include <string>
+#include <windows.h>
 
 using namespace Azure::Core::Http;
 
@@ -13,49 +14,121 @@ WinHttpTansport::WinHttpTansport() : HttpTransport() {}
 
 WinHttpTansport::~WinHttpTansport() {}
 
+std::string HttpMethodName(HttpMethod method)
+{
+  // This string should be all uppercase.
+  // Many servers treat HTTP verbs as case-sensitive, and the Internet Engineering Task Force (IETF)
+  // Requests for Comments (RFCs) spell these verbs using uppercase characters only.
+  switch (method)
+  {
+    case Get:
+      return "GET";
+    case Head:
+      return "HEAD";
+    case Post:
+      return "POST";
+    case Put:
+      return "PUT";
+    case Delete:
+      return "DELETE";
+    case Patch:
+      return "PATCH";
+    default:
+      throw Azure::Core::Http::TransportException("Invalid HTTP method.");
+  }
+}
+
 std::unique_ptr<RawResponse> WinHttpTansport::Send(Context const& context, Request& request)
 {
   void(context);
-  void(request);
 
   DWORD dwSize = 0;
   DWORD dwDownloaded = 0;
   LPSTR pszOutBuffer;
-  BOOL bResults = FALSE;
-  HINTERNET hSession = NULL, hConnect = NULL, hRequest = NULL;
+  BOOL sendResult = FALSE;
+  HINTERNET connectionHandle = NULL, requestHandle = NULL;
 
   // Use WinHttpOpen to obtain a session handle.
   // The dwFlags is set to 0 - all WinHTTP functions are performed synchronously.
-  hSession = WinHttpOpen(
+  HINTERNET sessionHandle = WinHttpOpen(
       L"WinHTTP Example/1.0",
       WINHTTP_ACCESS_TYPE_NO_PROXY,
       WINHTTP_NO_PROXY_NAME,
       WINHTTP_NO_PROXY_BYPASS,
       0);
 
-  // TODO: Get port and hostname from Url
+  // TODO: Get port from Url
   // Specify an HTTP server.
-  if (hSession)
-    hConnect = WinHttpConnect(hSession, L"www.microsoft.com", INTERNET_DEFAULT_PORT, 0);
+  if (sessionHandle)
+  {
+    // Uses port 80 for HTTP and port 443 for HTTPS.
+    // This function always operates synchronously.
+    connectionHandle = WinHttpConnect(
+        sessionHandle, request.GetUrl().GetHost().c_str(), INTERNET_DEFAULT_PORT, 0);
+  }
+  else
+  {
+    // ERROR_WINHTTP_INTERNAL_ERROR OR ERROR_NOT_ENOUGH_MEMORY
+    throw Azure::Core::Http::TransportException(
+        "Error while sending request. Error Code: " + std::to_string(GetLastError()));
+  }
 
   // Create an HTTP request handle.
-  if (hConnect)
-    hRequest = WinHttpOpenRequest(
-        hConnect,
-        L"GET",
+  if (connectionHandle)
+  {
+    requestHandle = WinHttpOpenRequest(
+        connectionHandle,
+        HttpMethodName(request.GetMethod()).c_str(),
         NULL,
-        NULL,
+        NULL, // Use HTTP/1.1
         WINHTTP_NO_REFERER,
-        WINHTTP_DEFAULT_ACCEPT_TYPES,
-        WINHTTP_FLAG_SECURE);
+        WINHTTP_DEFAULT_ACCEPT_TYPES, // No media types are accepted by the client
+        WINHTTP_FLAG_SECURE); // Uses secure transaction semantics (SSL/TLS)
+  }
+  else
+  {
+    DWORD error = GetLastError();
+
+    WinHttpCloseHandle(sessionHandle);
+
+    // Errors include:
+    // ERROR_WINHTTP_INCORRECT_HANDLE_TYPE
+    // ERROR_WINHTTP_INTERNAL_ERROR
+    // ERROR_WINHTTP_INVALID_URL
+    // ERROR_WINHTTP_OPERATION_CANCELLED
+    // ERROR_WINHTTP_UNRECOGNIZED_SCHEME
+    // ERROR_WINHTTP_SHUTDOWN
+    // ERROR_NOT_ENOUGH_MEMORY
+    throw Azure::Core::Http::TransportException(
+        "Error while sending request. Error Code: " + std::to_string(error));
+  }
 
   // Send a request.
-  if (hRequest)
-    bResults = WinHttpSendRequest(
-        hRequest, WINHTTP_NO_ADDITIONAL_HEADERS, 0, WINHTTP_NO_REQUEST_DATA, 0, 0, 0);
+  if (requestHandle)
+  {
+    sendResult = WinHttpSendRequest(
+        requestHandle, WINHTTP_NO_ADDITIONAL_HEADERS, 0, WINHTTP_NO_REQUEST_DATA, 0, 0, 0);
+  }
+  else
+  {
+    DWORD error = GetLastError();
+
+    WinHttpCloseHandle(connectionHandle);
+    WinHttpCloseHandle(sessionHandle);
+
+    // Errors include:
+    // ERROR_WINHTTP_INCORRECT_HANDLE_TYPE
+    // ERROR_WINHTTP_INTERNAL_ERROR
+    // ERROR_WINHTTP_INVALID_URL
+    // ERROR_WINHTTP_OPERATION_CANCELLED
+    // ERROR_WINHTTP_UNRECOGNIZED_SCHEME
+    // ERROR_NOT_ENOUGH_MEMORY
+    throw Azure::Core::Http::TransportException(
+        "Error while sending request. Error Code: " + std::to_string(error));
+  }
 
   // End the request.
-  if (bResults)
+  if (sendResult)
     bResults = WinHttpReceiveResponse(hRequest, NULL);
 
   // TODO: Call WinHttpQueryHeaders - after the receive response
@@ -108,7 +181,5 @@ std::unique_ptr<RawResponse> WinHttpTansport::Send(Context const& context, Reque
   if (hSession)
     WinHttpCloseHandle(hSession);
 
-
   throw;
 }
-
